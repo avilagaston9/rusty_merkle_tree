7ba19ac300529006e0f5216aace105512dccf9eb
Gaston <gaston@Gastons-MacBook-Pro.local>
2024-02-14 12:44:38 -0300

    Change the implementation to a simple vec of strings

--- merkle_tree/src/merkle_tree.rs
+++ merkle_tree/src/merkle_tree.rs
@@ -4,8 +4,6 @@ mod node;
 
 use crypto::digest::Digest;
 use crypto::sha3::Sha3;
-use node::Node;
-use std::rc::Rc;
 
 #[derive(Debug, PartialEq)]
 pub enum CreationError {
@@ -16,7 +14,7 @@ pub enum CreationError {
 }
 
 pub struct MerkleTree {
-    root: Rc<Node>,
+    leaves: Vec<String>,
 }
 impl MerkleTree {
     pub fn build_from(array: Vec<String>) -> Result<MerkleTree, CreationError> {
@@ -28,46 +26,40 @@ impl MerkleTree {
             return Err(CreationError::NotPowerOfTwo);
         }
 
-        let array_of_leaves = Self::get_leaves_nodes(&array);
+        let leaves = Self::get_leaves(&array);
 
-        let root = Self::build_tree(array_of_leaves);
-
-        Ok(MerkleTree { root })
+        Ok(MerkleTree { leaves })
     }
 
-    pub fn get_root_hash(&self) -> String {
-        self.root.get_hash()
-    }
+    pub fn calculate_root(&self) -> String { Self::calculate_root_from_array(&self.leaves)}
 
-    fn get_leaves_nodes(array: &[String]) -> Vec<Rc<Node>> {
+    fn get_leaves(array: &[String]) -> Vec<String> {
         let mut hasher = Sha3::keccak256();
 
-        // Use map to transform each element and collect the results into a new vector
-        let hashes: Vec<Rc<Node>> = array
+        let hashes: Vec<String> = array
             .iter()
             .map(|elem| {
                 hasher.input(elem.as_bytes());
-                Rc::new(Node::new_with_hash(hasher.result_str().to_string()))
+                hasher.result_str().to_string()
             })
             .collect();
 
         hashes
     }
 
-    fn build_tree(mut array: Vec<Rc<Node>>) -> Rc<Node> {
+    fn calculate_root_from_array(array: &Vec<String>) -> String {
         if array.len() == 1 {
-            return array.pop().unwrap();
+            return array.first().unwrap().clone();
         }
 
+        let mut hasher = Sha3::keccak256();
         let mut parents_array = vec![];
-
+        
         for chunk in array.chunks(2) {
-            parents_array.push(Rc::new(Node::new_parent(
-                Rc::clone(&chunk[0]),
-                Rc::clone(&chunk[1]),
-            )));
+            hasher.input((chunk[0].to_string() + &chunk[1].as_str()).as_bytes());
+            parents_array.push(hasher.result_str().to_string());
         }
 
-        return Self::build_tree(parents_array);
+        return Self::calculate_root_from_array(&parents_array);
     }
 }
